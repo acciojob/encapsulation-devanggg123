d75bb10f9f60816bf4a72531b0dbe6e8d3cea961
devanggg123 <sfdc.devang123@gmail.com>
2022-12-15 16:00:30 +0530

    hogya

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String s="name";
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS() {
+        this.s = s;
+    }
 }
