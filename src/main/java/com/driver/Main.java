db43855d224378536d02fb00119fabdb1a46fd54
devanggg123 <114430661+devanggg123@users.noreply.github.com>
2022-12-15 16:59:39 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,8 @@ package com.driver;
 public class Main {
   public static void main(String[] args){
       RWOnly obj=new RWOnly();
-      System.out.println(obj.getS());
+      obj.setName("Deva");
+      System.out.println(obj.getName());
 
   }
 }
